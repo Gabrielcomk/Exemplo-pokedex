39c11f6561fe9e085ec9cca1bb537aae37027b9b
Gabriel K <gabrielkarkotli@live.com>
2025-10-02 10:51:02 -0300

    Add Try Catch

--- podedex/src/Main.java
+++ podedex/src/Main.java
@@ -1,5 +1,5 @@
-
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 import java.io.IOException;
 import java.net.URI;
@@ -9,8 +9,7 @@ import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Busque seu pokémon favorito: ");
@@ -18,32 +17,40 @@ public class Main {
 
         String url = "https://pokeapi.co/api/v2/pokemon/" + search;
 
-            // Criando a conexão HTTP
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .GET()
-                .build();
+        try {
+            //conexão
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
 
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
+            if (response.statusCode() != 200) {
+                System.out.println("Erro: Pokémon não encontrado ou problema na API (código " + response.statusCode() + ")");
+                return;
+            }
 
-        String json = response.body();
-        Gson gson = new Gson();
+            String json = response.body();
+            Gson gson = new Gson();
 
-        //convertendo o JSON para objeto usando o GSON
-        PokemonAtributos meuPokemon = gson.fromJson(json, PokemonAtributos.class);
+            // Convertendo o JSON para objeto usando o GSON
+            PokemonAtributos meuPokemon = gson.fromJson(json, PokemonAtributos.class);
 
+            // Exibe as info
+            System.out.println("Name: " + meuPokemon.getName());
+            System.out.println("ID: " + meuPokemon.getId());
+            System.out.println("Tipo: " + meuPokemon.getTypes());
+            System.out.println("Info: " + meuPokemon.getStats());
 
-
-
-        System.out.println("Name: " + meuPokemon.getName());
-        System.out.println("ID: " + meuPokemon.getId());
-        System.out.println("Tipo: " + meuPokemon.getTypes());
-        System.out.println("Info: " + meuPokemon.getStats());
-
-
-
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Erro de conexão: " + e.getMessage());
+        } catch (JsonSyntaxException e) {
+            System.out.println("Erro ao interpretar o JSON da API.");
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro inesperado: " + e.getMessage());
+        }
     }
 }
